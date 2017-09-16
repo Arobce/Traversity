992769f8a2a461ca7434db1445bfe9e11ee29cf4
Roshan Chapagain <arobce@gmail.com>
2017-09-16 17:38:17 +0545

    Added users current location

--- app/src/main/java/com/roshan/traversity/RouteMapFragment.java
+++ app/src/main/java/com/roshan/traversity/RouteMapFragment.java
@@ -1,8 +1,19 @@
 package com.roshan.traversity;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -27,6 +38,9 @@ public class RouteMapFragment extends Fragment implements OnMapReadyCallback {
     GoogleMap mgoogleMap;
     MapView mapView;
     View mView;
+    LocationListener locationListener;
+    LocationManager locationManager;
+
 
     @Nullable
     @Override
@@ -35,6 +49,21 @@ public class RouteMapFragment extends Fragment implements OnMapReadyCallback {
         return mView;
     }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode==1){
+
+            if(grantResults[0]==PackageManager.PERMISSION_GRANTED){
+
+                if(ContextCompat.checkSelfPermission(getContext(),Manifest.permission.ACCESS_FINE_LOCATION)==PackageManager.PERMISSION_GRANTED){
+                    locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,0,0,locationListener);
+
+                }
+            }
+
+        }
+    }
 
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
@@ -61,7 +90,61 @@ public class RouteMapFragment extends Fragment implements OnMapReadyCallback {
         LatLng everest = new LatLng(27.9878493, 86.9162713);
 
         mgoogleMap.addMarker(new MarkerOptions().position(everest).title("Liberty").snippet("Lets go"));
-        mgoogleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(everest,10));
+//        mgoogleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(everest,10));
+
+
+        //get current lat lng
+        locationManager = (LocationManager) getContext().getSystemService(Context.LOCATION_SERVICE);
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+
+                LatLng currLocation = new LatLng(location.getLatitude(),location.getLongitude());
+                mgoogleMap.addMarker(new MarkerOptions().position(currLocation).title("Your current location"));
+                mgoogleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(currLocation,12));
+            }
+
+            @Override
+            public void onStatusChanged(String s, int i, Bundle bundle) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String s) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String s) {
+
+            }
+        };
+
+        if(Build.VERSION.SDK_INT < 23){
+
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,0,0,locationListener);
+
+            Location lastKnownLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+
+            LatLng currLocation = new LatLng(lastKnownLocation.getLatitude(),lastKnownLocation.getLongitude());
+            mgoogleMap.addMarker(new MarkerOptions().position(currLocation).title("Your current location"));
+            mgoogleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(currLocation,12));
+
+        }else{
+
+            if (ContextCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+                ActivityCompat.requestPermissions(getActivity(),new String[]{Manifest.permission.ACCESS_FINE_LOCATION},1);
+            }else{
+
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,0,0,locationListener);
+
+            }
+
+        }
+
+
+
 
     }
 }
