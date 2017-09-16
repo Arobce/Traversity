753e65f5b4421d41ed1322bb5f547508648f53a7
Roshan Chapagain <arobce@gmail.com>
2017-09-16 16:49:31 +0545

    Added Maps Fragment

--- app/src/main/java/com/roshan/traversity/RouteMapFragment.java
+++ app/src/main/java/com/roshan/traversity/RouteMapFragment.java
@@ -7,17 +7,61 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
 /**
  * Created by Roshan on 9/14/2017.
  */
 
-public class RouteMapFragment extends Fragment {
+public class RouteMapFragment extends Fragment implements OnMapReadyCallback {
+
 
     public static final String TAG = "routeMapsFragment";
+    GoogleMap mgoogleMap;
+    MapView mapView;
+    View mView;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.route_maps_fragment,container,false);
-        return view;
+        mView = inflater.inflate(R.layout.route_maps_fragment,container,false);
+        return mView;
+    }
+
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        mapView= (MapView) mView.findViewById(R.id.map);
+
+        if(mapView!=null){
+
+            mapView.onCreate(null);
+            mapView.onResume();
+            mapView.getMapAsync(this);
+        }
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
+        MapsInitializer.initialize(getContext());
+
+        mgoogleMap=googleMap;
+        mgoogleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+
+        LatLng everest = new LatLng(27.9878493, 86.9162713);
+
+        mgoogleMap.addMarker(new MarkerOptions().position(everest).title("Liberty").snippet("Lets go"));
+        mgoogleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(everest,10));
+
     }
 }
