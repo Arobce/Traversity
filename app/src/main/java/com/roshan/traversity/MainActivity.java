8e3c053778f9c1f4d4914f0dbb1b69165b5447e2
Rohan Shrestha <sth.ronx@gmail.com>
2018-01-23 13:52:29 +0545

    Modified side navbar

--- app/src/main/java/com/roshan/traversity/MainActivity.java
+++ app/src/main/java/com/roshan/traversity/MainActivity.java
@@ -183,13 +183,16 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        if (id == R.id.nav_home) {
 
-        } else if (id == R.id.nav_slideshow) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_nearby) {
+
+
+
+        } else if (id == R.id.nav_events) {
+
+        } else if (id == R.id.nav_tools) {
 
         } else if (id == R.id.nav_share) {
 
